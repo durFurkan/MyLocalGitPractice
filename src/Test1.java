ce0dec48583b9f5a5d125b59aaed4ff0a55aeaf4
Furkan Dur <furkandur.95@gmail.com>
2023-09-18 20:26:24 +0100

    Update Test1.java via remote repo

--- src/Test1.java
+++ src/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
         System.out.println("Test1");
         System.out.println("ADD IN LOCAL!!");
         System.out.println("ADDED IN LOCAL, WITH PUSH MENU!!");
+        // BURASI REMOTE REPODA GITHUB DA EKLENDI!!!!!
     }
 }
